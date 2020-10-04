3eecf50366524f4a79805ae199c10cb2b7038c3f
Shahrukh Mullani <mullanishah@gmail.com>
2020-10-04 20:16:23 +0530

    first comment

---
+++ src/main/java/com/app/tasha/projectabstractservice/ProjectAbstractServiceApplication.java
@@ -0,0 +1,13 @@
+package com.app.tasha.projectabstractservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectAbstractServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectAbstractServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/tasha/projectabstractservice/ProjectAbstractServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.tasha.projectabstractservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectAbstractServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
